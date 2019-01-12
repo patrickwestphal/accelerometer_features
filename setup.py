0114c6bb6537c1343cd61c89b2548598b2b3e9b1
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2019-01-12 16:28:46 +0100

    Added dependencies and declared packages
    ...in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from distutils.core import setup
 setup(
     name='accelerometer_features_evaluation',
     version='0.0.3',
-    packages=[''],
+    packages=[
+        'accelerometerfeatures.utils',
+        'accelerometerfeatures.utils.pytorch',
+    ],
     url='',
     license='',
     author='Patrick Westphal',
@@ -14,5 +17,7 @@ setup(
         'scipy==1.1.0',
         'pandas==0.23.4',
         'numpy==1.15.4',
+        'torch==1.0.0',
+        'torchvision==0.2.1',
     ]
 )
